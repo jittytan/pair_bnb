897611fa7e2a35e3a50a8be44ce65648aada1522
Tan Jit SIang <joetjsjoe@hotmail.com>
2016-10-17 15:08:47 +0800

    completed omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,9 @@ class User < ActiveRecord::Base
 
   def self.create_with_auth_and_hash(authentication,auth_hash)
     create! do |u|
+
       u.first_name = auth_hash["info"]["first_name"]
+      u.password = SecureRandom.hex(4)
       u.email = auth_hash["extra"]["raw_info"]["email"]
       u.authentications<<(authentication)
     end
