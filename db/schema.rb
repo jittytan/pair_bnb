d871f0688d4157cd92d18281e6783793803a781e
Tan Jit SIang <joetjsjoe@hotmail.com>
2016-10-18 14:04:15 +0800

    migrate listing file

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161017064001) do
+ActiveRecord::Schema.define(version: 20161018052953) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,12 @@ ActiveRecord::Schema.define(version: 20161017064001) do
     t.string   "token"
     t.string   "provider"
     t.string   "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "listings", force: :cascade do |t|
+    t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
