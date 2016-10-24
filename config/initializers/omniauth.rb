4cde112fd9b0cac2643f8a80a3fab56e3469719c
Tan Jit SIang <joetjsjoe@hotmail.com>
2016-10-24 10:04:03 +0800

    change the omniauth so that key and secret is in application.yml

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '206242543138832', 'a9929255ca1652eab82a8e2fc004e12b', info_fields: 'email, first_name, last_name'
+  provider :facebook, ENV["facebook_key"], ENV["facebook_secret"], info_fields: 'email, first_name, last_name'
 end
